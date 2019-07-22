b6193e77e54febd520b3bb26a5b0c9056deb23c1
Utepov Timur <t.utepov@hotmail.com>
2019-07-22 09:43:39 +0100

    Updates

---
+++ precache-manifest.d094dc43b09cea2acef4118d739696de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8354b3fc86f54476ce245a089b6821d",
+    "url": "/pwa-app/index.html"
+  },
+  {
+    "revision": "831151484cb97dc78775",
+    "url": "/pwa-app/static/css/main.1d6a118a.chunk.css"
+  },
+  {
+    "revision": "c88e7946fcbfdd85b961",
+    "url": "/pwa-app/static/js/2.accf2d20.chunk.js"
+  },
+  {
+    "revision": "831151484cb97dc78775",
+    "url": "/pwa-app/static/js/main.7c4802be.chunk.js"
+  },
+  {
+    "revision": "831d68a4fa5cee718a37",
+    "url": "/pwa-app/static/js/runtime~main.8d2502ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwa-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
